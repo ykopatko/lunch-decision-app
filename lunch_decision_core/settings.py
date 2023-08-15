e47dca88480464d5e3ce923fdb139df86ca9ecde
ykopatko <cs.kopatko.evgen@gmail.com>
2023-08-15 13:32:49 +0300

    Fix typos in settings.py

--- lunch_decision_core/settings.py
+++ lunch_decision_core/settings.py
@@ -60,9 +60,9 @@ REST_FRAMEWORK = {
 
 
 JWT_AUTH = {
-    'JWT_ALLOW_REFRESH': True,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=1),
-    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+    "JWT_ALLOW_REFRESH": True,
+    "JWT_EXPIRATION_DELTA": datetime.timedelta(days=1),
+    "JWT_REFRESH_EXPIRATION_DELTA": datetime.timedelta(days=7),
 }
 
 
